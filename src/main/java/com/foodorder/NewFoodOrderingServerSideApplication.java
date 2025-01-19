4c50696bf91623c75f0971b592b8d74b42233cb0
nayajek <ifunanya.ejeckam@keyin.com>
2025-01-19 17:08:56 -0330

    Initial commit

---
+++ src/main/java/com/foodorder/NewFoodOrderingServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.foodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFoodOrderingServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFoodOrderingServerSideApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/foodorder/dto/OrderDetailsDto.java
@@ -0,0 +1,4 @@
+package com.foodorder.dto;
+
+public class OrderDetailsDto {
+}
